8c35c36ef9db7432bae4780d41507516227c2978
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-11-24 15:59:06 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/JobListings.jsx 	new file:   src/components/Spinner.jsx

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import jobs from '../jobs.json'
-import JobListing from './JobListing'
+import { useState, useEffect } from "react";
+import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
-const JobListings = ({ isHome = false }) => { 
-    const JobListings = isHome ? jobs.slice(0,3) : jobs;
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log(error, "Error in fetching data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchJobs();
+    }, 2000);
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          { isHome ? "Recent Jobs" : "Browse Jobs" }
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-
-            {JobListings.map((job)=>(
-                <JobListing key={job.id} job={job} />
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : 
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job)=>(
+            <JobListing key={job.id} job={job} />
             ))}
-          
-        </div>
+          </div>
+        }
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default JobListings
+export default JobListings;
