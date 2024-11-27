8c1ce7053fd00ab99799309aef90dc57727f3352
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-11-27 23:44:21 +0530

    modified:   src/pages/AddJob.jsx

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -11,13 +11,20 @@ const AddJob = () => {
     const [comapnyEmail, setComapnyEmail] = useState('')
     const [contactPhone, setContactPhone] = useState('')
 
+    const submitForm = (e) => {
+      e.preventDefault();
+
+      console.log(description);
+      
+    }
+
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form>
+          <form onSubmit={submitForm}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
