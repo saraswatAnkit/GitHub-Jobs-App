589240bc11f6ac5b293824ba4b98650bee3c210c
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-11-28 17:08:54 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://react-job-api-backend.vercel.app/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       } 
